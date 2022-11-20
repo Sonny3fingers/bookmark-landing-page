75cf09f15955f4e63278bcedbf5f3b4ea0744c28
Sonny3fingers <77343092+Sonny3fingers@users.noreply.github.com>
2022-11-20 22:15:31 +0100

    added typography and variables files to sass

--- src/js/main.js
+++ src/js/main.js
@@ -19,8 +19,6 @@ const featuresImgDOM_2 = document.querySelector(".features-img-2");
 const featuresImgDOM_3 = document.querySelector(".features-img-3");
 const featuresSlides = document.querySelectorAll(".slide");
 
-// const slides = document.querySelectorAll(".slide");
-
 // features list add active class to item
 featuresList.addEventListener("click", (e) => {
   if (!e.target.classList.contains("active")) {
@@ -35,29 +33,8 @@ featuresList.addEventListener("click", (e) => {
         slide.classList.remove("active");
       }
     }
-  } else {
-    return;
   }
 });
-// features section
-// list.addEventListener("click", (event) => {
-//   if (event.target.classList.contains("active")) {
-//     return;
-//   } else {
-//     for (const item of items) {
-//       item.classList.remove("active");
-//     }
-//     event.target.classList.add("active");
-//     slides.forEach((slide) => {
-//       if (event.target.className.includes(slide.id)) {
-//         slide.classList.add("active");
-//       } else {
-//         slide.classList.remove("active");
-//       }
-//     });
-//   }
-// });
-
 // Add hero image
 window.addEventListener("DOMContentLoaded", () => {
   provideDOMImage(heroImage, heroImageDIV);
@@ -67,16 +44,10 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 // provide DOM images
 function provideDOMImage(img, elementDOM) {
-  // Check for DIV dom element or img dom element
-  if (elementDOM.tagName === "DIV") {
-    const image = document.createElement("img");
-    image.src = img;
-    image.classList.add("img-fluid");
-    elementDOM.append(image);
-  } else {
-    elementDOM.src = img;
-    elementDOM.classList.add("img-fluid");
-  }
+  const image = document.createElement("img");
+  image.src = img;
+  image.classList.add("img-fluid");
+  elementDOM.append(image);
 }
 // Check for window resize and remove all reset all classes for mobile navigation
 window.addEventListener("resize", () => {
@@ -94,8 +65,12 @@ hamburgerMenu.addEventListener("click", () => {
   if (hamburgerMenu.classList.contains("is-active")) {
     mobileMenu.classList.remove("hide-mobile-menu");
     mobileMenu.classList.add("show-mobile-menu");
+    // Prevent scrolling
+    document.body.style.overflowY = "hidden";
   } else {
     mobileMenu.classList.add("hide-mobile-menu");
     mobileMenu.classList.remove("show-mobile-menu");
+    // Enable scrolling
+    document.body.style.overflowY = "visible";
   }
 });
